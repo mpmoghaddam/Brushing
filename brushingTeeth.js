6a9ba4fbfaab8cefcf2e538c24dc3931c8b5f10f
Moh Moghaddam <you@example.commpmoghaddam@yahoo.com>
2022-03-20 13:04:48 -0600

    Test

---
+++ brushingTeeth.js
@@ -0,0 +1,30 @@
+
+let toothpasteAvailable = true
+let waterAvailable = true
+
+console.log('start')
+console.log('do i have toothpaste available', toothpasteAvailable)
+    if(toothpasteAvailable){
+        console.log('do i have water available', waterAvailable)
+        if(waterAvailable){
+             console.log('put the toothpast on the toothbrush')
+             console.log('do the brushing')
+        }
+    }     
+    else{
+        console.log('put the toothpaste in the shopping list')
+        }
+console.log('end')
+         
+    
+
+        
+
+
+
+
+
+
+
+
+
